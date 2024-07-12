41200e13459c7f28056fee23e634204a3bcd10db
tnd18091995 <tnd18091995@gmail.com>
2024-07-12 11:30:12 +0700

    update

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function showListPromotion() {
                     '<td>' + promotion.discount + '</td>' +
                     '<td>' + promotion.detail + '</td>' +
                     '<td><button onclick="editPromotion(' + promotion.id + ')" class="btn btn-success"><i class="fas fa-edit"></i> Edit</button>' +
-                    ' <button onclick="deletePromotion(' + promotion.id + ')" class="btn btn-warning"><i class="fas fa-dollar-sign"></i> Delete</button></td>' +
+                    ' <button onclick="deletePromotion(' + promotion.id + ')" class="btn btn-danger"><i class="fas fa-dollar-sign"></i> Delete</button></td>' +
                     '</tr>';
                 $('#promotionTableBody').append(row);
             });
@@ -32,10 +32,8 @@ function showListPromotion() {
 
 function editPromotion(id) {
     console.log('Edit promotion with ID:', id);
-    // Thực hiện các thao tác chỉnh sửa khuyến mãi, ví dụ chuyển hướng đến trang chỉnh sửa
 }
 
 function deletePromotion(id) {
     console.log('Delete promotion with ID:', id);
-    // Thực hiện các thao tác xóa khuyến mãi, ví dụ gọi AJAX để xóa và cập nhật lại danh sách
 }
